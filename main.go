a467d564e4ac29b4b3245b68d725ef06143a78ea
Tamal Saha <tamal@appscode.com>
2020-11-28 04:42:23 -0800

    added-all

--- main.go
+++ main.go
@@ -128,6 +128,59 @@ func (si *SheetInfo) updateRowData(row int64, data []string, formatCell bool) er
 	return nil
 }
 
+func (si *SheetInfo) appendRowData(data []string, formatCell bool) error {
+	var format *sheets.CellFormat
+
+	if formatCell {
+		// for updating header color and making it bold
+		format = &sheets.CellFormat{
+			TextFormat: &sheets.TextFormat{
+				Bold: true,
+			},
+			BackgroundColor: &sheets.Color{
+				Alpha: 1,
+				Blue:  149.0 / 255.0,
+				Green: 226.0 / 255.0,
+				Red:   239.0 / 255.0,
+			},
+		}
+	}
+
+	vals := make([]*sheets.CellData, 0, len(data))
+	for i := range data {
+		vals = append(vals, &sheets.CellData{
+			UserEnteredFormat: format,
+			UserEnteredValue: &sheets.ExtendedValue{
+				StringValue: &data[i],
+			},
+		})
+	}
+
+	req := []*sheets.Request{
+		{
+			AppendCells: &sheets.AppendCellsRequest{
+				SheetId: si.CurrentSheetID,
+				Fields:  "*",
+				Rows: []*sheets.RowData{
+					{
+						Values: vals,
+					},
+				},
+			},
+		},
+	}
+	_, err := si.srv.Spreadsheets.BatchUpdate(si.SpreadSheetsID, &sheets.BatchUpdateSpreadsheetRequest{
+		IncludeSpreadsheetInResponse: false,
+		Requests:                     req,
+		ResponseIncludeGridData:      false,
+	}).Do()
+	if err != nil {
+		return fmt.Errorf("unable to update: %v", err)
+	}
+
+	return nil
+}
+
 func (si *SheetInfo) getSheetId(name string) (int64, error) {
 	resp, err := si.srv.Spreadsheets.Get(si.SpreadSheetsID).Do()
 	if err != nil {
@@ -224,11 +277,6 @@ func (si *SheetInfo) insertLicenseInfoInSheet(info LicenseInfo) error {
 		log.Fatal(err)
 	}
 
-	row, err := si.findEmptyCell()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	vals := []string{
 		"1",
 		info.Name,
@@ -236,5 +284,5 @@ func (si *SheetInfo) insertLicenseInfoInSheet(info LicenseInfo) error {
 		info.ClusterID,
 		time.Now().UTC().Format(time.RFC3339),
 	}
-	return si.updateRowData(row, vals, false)
+	return si.appendRowData(vals, false)
 }
